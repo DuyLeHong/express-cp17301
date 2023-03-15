9288490e34419816c0ed754bbad2d4efa28c83dd
Duy Le <lehongduy.bk@gmail.com>
2023-03-15 11:19:41 +0700

    update code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", function (req, res) {
 //   res.send('hello world')
 // })
 
-const port = 3000
+const port = 8000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
