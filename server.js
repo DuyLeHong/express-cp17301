7b58ae0dd00411656ea20340b8ead7a465340037
Duy Le <lehongduy.bk@gmail.com>
2023-03-15 15:56:50 +0700

    update code - use hbs

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const expressHbs = require('express-handlebars');
 const app = express()
 
 //app.engine('.hbs', ExpressHandlebars());
-app.engine('.hbs', expressHbs.engine({extname: "hbs"}));
+app.engine('.hbs', expressHbs.engine({extname: "hbs", defaultLayout: 'page2', layoutsDir: "views/layouts/"}));
+
+//app.engine( "hbs", engine({ extname: "hbs", defaultLayout: false, layoutsDir: "views/layouts/", }) );
 
 app.set('view engine', '.hbs');
 app.set('views', './views');
