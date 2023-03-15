457518c985785c983a3bac64e182f0323b87e3e0
Duy Le <lehongduy.bk@gmail.com>
2023-03-15 11:19:02 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+//app.use(express.static(__dirname));
+app.get("/", function (req, res) {
+    console.log(__dirname);
+    res.sendFile(__dirname + "/index.html");
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/', (req, res) => {
+//   res.send('hello world')
+// })
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
