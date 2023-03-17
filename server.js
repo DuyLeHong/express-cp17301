5dd5b7b18e847dc2af56fb209bcee43df546ed34
Duy Le <lehongduy.bk@gmail.com>
2023-03-17 14:51:04 +0700

    update code - hbs

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const expressHbs = require('express-handlebars');
 const app = express()
 
 //app.engine('.hbs', ExpressHandlebars());
-app.engine('.hbs', expressHbs.engine({extname: "hbs", defaultLayout: 'page2', layoutsDir: "views/layouts/"}));
+app.engine('.hbs', expressHbs.engine({ 
+  extname: "hbs", 
+  defaultLayout: 'main', 
+  layoutsDir: "views/layouts/" }));
 
 //app.engine( "hbs", engine({ extname: "hbs", defaultLayout: false, layoutsDir: "views/layouts/", }) );
 
@@ -14,14 +17,28 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    layout: 'main',
+    //showContentMaytinh: false,
+
+    helpers: {
+      foo() { return 'foo. CP17305 - server Android'; }
+    }
+  });
 });
 
-//app.use(express.static(__dirname));
-// app.get("/", function (req, res) {
-//     console.log(__dirname);
-//     res.sendFile(__dirname + "/index.html");
-// });
+app.get('/maytinh', (req, res) => {
+  res.render('emptyView', {
+    layout: 'main', 
+    showContentMaytinh: true,
+    soA: 15,
+    soB: 7,
+    phepTinh: 'cong',
+    kq: 22,
+
+  });
+});
+
 
 const port = 8000
 
